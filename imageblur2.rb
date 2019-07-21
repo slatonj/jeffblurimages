e7a1a29882bc4ddeb0de5fce328b9cbc19ac85a1
jeff <jeffslaton@msncom>
2019-07-21 20:44:12 +0000

    cleaned of the formatt

--- imageblur2.rb
+++ imageblur2.rb
@@ -9,22 +9,22 @@ class Image
         @image.each_with_index do |array, vert|
         array.each_with_index do |num, horz|
 
-          if num == 1
-            location << [vert, horz]
-          end
+        if num == 1
+        location << [vert, horz]
         end
       end
-     location
     end
+     location
+  end
 
   def blur
     ones = location
-          ones.each do |vert_index, horz_index|          
-            @image[vert_index][horz_index -1] = 1 unless horz_index == 0
-            @image[vert_index][horz_index +1] = 1 unless horz_index == 3
-            @image[vert_index -1][horz_index] = 1 unless vert_index == 0
-            @image[vert_index +1][horz_index] = 1 unless vert_index == 3
-          end        
+    ones.each do |vert_index, horz_index|          
+      @image[vert_index][horz_index -1] = 1 unless horz_index == 0
+      @image[vert_index][horz_index +1] = 1 unless horz_index == 3
+      @image[vert_index -1][horz_index] = 1 unless vert_index == 0
+      @image[vert_index +1][horz_index] = 1 unless vert_index == 3
+    end      
   end
   
   
@@ -43,10 +43,10 @@ image = Image.new([
         [0, 0, 0, 1],
         [0, 0, 0, 0]
 			])
-			image.output_image
-			puts
-			image.blur
+image.output_image
+puts
+image.blur
 
-      image.output_image
-			
+image.output_image
+
 			
