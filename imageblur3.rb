2c4f6c7a6c270e90f87029af56dfc41e284330a2
jeff <jeffslaton@msncom>
2019-08-04 18:05:06 +0000

    done with imageblur

---
+++ imageblur3.rb
@@ -0,0 +1,92 @@
+class Image
+
+	def initialize(image)
+		@image = image
+	end
+
+    def location
+        location = []
+        @image.each_with_index do |array, vert|
+        array.each_with_index do |num, horz|
+
+        if num == 1
+        location << [vert, horz]
+        end
+      end
+    end
+     location
+  end
+
+  def blur (distance)
+    distance.times do 
+    ones = location
+    ones.each do |vert_index, horz_index|          
+      @image[vert_index][horz_index - 1] = 1 unless horz_index == 0 # right
+      @image[vert_index][horz_index + 1] = 1 unless horz_index == @image[0].length  - 1 # left
+      @image[vert_index - 1][horz_index] = 1 unless vert_index == 0 # up
+      @image[vert_index + 1][horz_index] = 1 unless vert_index == @image.length  - 1 # down
+    end
+  end    
+  end
+  
+  
+  
+  def output_image
+	  @image.each do |x|
+		 puts x.join(" ") 
+		end
+  end  
+
+end
+
+    
+image = Image.new([
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1]
+])
+image.output_image
+puts
+image.blur(5)
+image.output_image
+
+
+puts
+
+
+image2 = Image.new([
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+  ])
+image2.output_image
+puts
+image2.blur(3)
+
+image2.output_image
+
+			
